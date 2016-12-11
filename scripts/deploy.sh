c87f7d57a8b264c23036c4495ce9aa8a35b43a14
Patrick Skjennum <mail@habitats.no>
2016-12-11 18:52:30 +0000

    fix: update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ fi
 cd /home/habitats/rms && git reset --hard HEAD && git fetch && git pull origin $branch && git checkout $branch
 cd /home/habitats/rms/scripts && chmod +x * && cd ..
 ./gradlew stage
-if [[ $branch == "demo" ]]; then
+if [[ $branch == "stage" ]]; then
   mv /home/habitats/rms/rms_*war /home/habitats/rms/rms-demo.war
   cp -f /home/habitats/rms/rms-demo.war /opt/tomcat/demo/ROOT.war
 else
