5030d451dee381d3ec1f5873de51e1459193386f
Patrick Skjennum <mail@habitats.no>
2016-12-11 17:13:15 +0000

    conf: upgrade wrapper

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ else
 fi
 cd /home/habitats/rms && git reset --hard HEAD && git fetch && git pull origin $branch && git checkout $branch
 cd /home/habitats/rms/scripts && chmod +x * && cd ..
-./gradlew stage
+gradle stage
 if [[ $branch == "demo" ]]; then
   mv /home/habitats/rms/rms_*war /home/habitats/rms/rms-demo.war
   cp -f /home/habitats/rms/rms-demo.war /opt/tomcat/demo/ROOT.war
