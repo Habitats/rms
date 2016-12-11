badd344422065ce580c5d8f01ded84e10913ffe1
Patrick Skjennum <mail@habitats.no>
2016-12-11 18:05:03 +0100

    conf: naming and deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+let branch
+if [[ $1 == "prod" ]]; then
+  branch="master"
+else
+  branch="stage"
+fi
+cd /home/habitats/rms && git reset --hard HEAD && git fetch && git pull origin $branch && git checkout $branch
+cd /home/habitats/rms/scripts && chmod +x * && cd ..
+./gradlew stage
+if [[ $branch == "demo" ]]; then
+  mv /home/habitats/rms/rms_*war /home/habitats/rms/rms-demo.war
+  cp -f /home/habitats/rms/rms-demo.war /opt/tomcat/demo/ROOT.war
+else
+  cp -f /home/habitats/rms/rms_*war /opt/tomact/prod/ROOT.war
+fi
