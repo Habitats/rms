39dfa1fcf5fb6df0b6564a01cd14a00c00a60016
Patrick Skjennum <mail@habitats.no>
2016-12-27 23:57:00 +0100

    fix: spelling

--- frontend/app/containers/About.jsx
+++ frontend/app/containers/About.jsx
@@ -41,16 +41,16 @@ export default class About extends Component {
           <TextBox>
             <div>
               <p> Romerike Markiseservice AS ble etablert i 1986 i Maura av Morten og Roar Skjennum. Sammen på laget hadde de en tredje
-                bror, Bjarne, som montør samt Anne Grethe med det praktiske kontoransvaret. I dag har bedriften vokst til totalt seks
+                bror, Bjarne, som montør, samt Anne Grethe med det praktiske kontoransvaret. I dag har bedriften vokst til totalt seks
                 ansatte, alle med et meget høyt kompetansenivå innenfor sine respektive områder.</p>
 
-              <p> Gjennom mer enn 30 års erfaring har bedriften etterhvert etablert seg til å bli en totalleverandør av solskjerming til
+              <p> Gjennom mer enn 30 års erfaring har bedriften etter hvert etablert seg til å bli en totalleverandør av solskjerming til
                 både privat- og bedriftsmarkedet, og opererer som en offisiell forhandler av produkter fra <em>Hunter Douglas
                   Solskjerming</em> gjennom et samarbeid med <em> <a href="http://www.kjellsmarkiser.no/" target="_blank">Kjells
                   Markiser</a></em>.</p>
 
-              <p> Vårt salgsområde tar utgangspunkt i Øvre Romerike, men vi gjennomfører også oppdrag utenfor disse. Som kunde hos oss kan
-                du til enhver tid forvente produkter av høy standard og rask levering, samt kvalitetssikret av våre egne montører. I tillegg
+              <p> Vårt salgsområde tar utgangspunkt i Øvre Romerike, men vi gjennomfører også oppdrag i andre regioner. Som kunde hos oss kan
+                du til enhver tid forvente produkter av høy standard og kort med leveringstid – alt kvalitetssikret av egne montører. I tillegg
                 har vi gjennom årene opparbeidet oss en særegen ekspertise innen service og vedlikehold av næringsbygg.</p>
               <br />
             </div>
