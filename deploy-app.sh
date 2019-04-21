c53abea72f49020a3f6c9b6be36fc15057e83fd9
Patrick Skjennum <mail@habitats.no>
2019-04-22 00:12:52 +0200

    feat: working persitent storage

--- deploy-app.sh
+++ deploy-app.sh
@@ -2,9 +2,9 @@
 kubectl delete -f kube/volumes/deployment.yaml
 kubectl delete -f kube/app/volume.yaml
 kubectl delete -f kube/app/deployment.yaml
-#kubectl delete -f kube/db/pv.yaml
+#kubectl delete -f kube/db/dropbox-cfg-pv.yaml
 
 kubectl create -f kube/volumes/deployment.yaml
 kubectl create -f kube/app/volume.yaml
 kubectl create -f kube/app/deployment.yaml
-#kubectl create -f kube/db/pv.yaml
+#kubectl create -f kube/db/dropbox-cfg-pv.yaml
