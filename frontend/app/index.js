927c2c7b801310c4af910154b3d968f94992308a
Patrick Skjennum <mail@habitats.no>
2025-04-02 13:50:10 +0200

    fix: styled compoenents validator

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, StyleSheetManager } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
 import { router } from './routes'
 import theme from './theme'
 import GlobalStyle from './GlobalStyle'
@@ -21,15 +22,24 @@ const queryClient = new QueryClient({
   },
 })
 
+// Custom shouldForwardProp function
+const shouldForwardProp = prop => {
+  // List of props that should not be forwarded to DOM elements
+  const propsToFilter = ['isSmall', 'isMedium'];
+  return isPropValid(prop) && !propsToFilter.includes(prop);
+};
+
 // Render the app
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </StyleSheetManager>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
